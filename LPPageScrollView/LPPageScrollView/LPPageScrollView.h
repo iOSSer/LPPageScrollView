c9d0c323671f8dbcb9c28564b98a7f693bc0bb9e
LiPeng <995139593@qq.com>
2015-08-19 10:05:56 +0800

    ~~

---
+++ LPPageScrollView/LPPageScrollView/LPPageScrollView.h
@@ -0,0 +1,13 @@
+//
+//  LPPageScrollView.h
+//  LPPageScrollView
+//
+//  Created by MacBook on 15/8/19.
+//  Copyright (c) 2015年 LiPeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LPPageScrollView : UIScrollView
+
+@end
---
+++ LPPageScrollView/LPPageScrollView/LPPageScrollView.m
@@ -0,0 +1,21 @@
+//
+//  LPPageScrollView.m
+//  LPPageScrollView
+//
+//  Created by MacBook on 15/8/19.
+//  Copyright (c) 2015年 LiPeng. All rights reserved.
+//
+
+#import "LPPageScrollView.h"
+
+@implementation LPPageScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
